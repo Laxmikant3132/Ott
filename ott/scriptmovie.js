dd04a95f91ff8397d47e169df935d1cfcb7f7250
Laxmikant3132 <laxmikanttalli303@gmail.com>
2025-04-26 22:42:56 +0530

    Add files via upload

---
+++ ott/scriptmovie.js
@@ -0,0 +1,5 @@
+// Optional: form submission or content interaction
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    alert('Thanks for reaching out! We will get back to you soon.');
+  });
+  
